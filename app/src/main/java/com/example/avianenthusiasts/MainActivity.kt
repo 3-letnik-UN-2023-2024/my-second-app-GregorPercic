c64bdb8d2e6eda9eccff82c8ffa45d217376a501
unknown <gregapercic000@gmail.com>
2023-12-18 20:36:42 +0100

    added notifications (N08 completed)

--- app/src/main/java/com/example/avianenthusiasts/MainActivity.kt
+++ app/src/main/java/com/example/avianenthusiasts/MainActivity.kt
@@ -12,15 +12,23 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.location.LocationListener
 import android.location.LocationManager
+import android.os.Build
 import android.preference.PreferenceManager
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 import com.example.avianenthusiasts.databinding.ActivityMainBinding
 import org.osmdroid.config.Configuration
+import org.osmdroid.library.BuildConfig
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.CustomZoomButtonsController
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
 
@@ -45,12 +53,13 @@ class MainActivity : AppCompatActivity() {
                     longitude = lastKnownLongitude ?: 0.0
                 )
 
-                Toast.makeText(this, "${newBird.latitude}, ${newBird.longitude}", Toast.LENGTH_SHORT).show()
+                //Toast.makeText(this, "${newBird.latitude}, ${newBird.longitude}", Toast.LENGTH_SHORT).show()
 
                 app.loadFromFile()
                 app.addItem(newBird)
 
                 addMarker(newBird)
+                checkAndNotifyForFrequentBirds(newBird.species)
             }
         }
 
@@ -62,6 +71,7 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+        private const val NOTIFICATION_PERMISSION_REQUEST_CODE = 101
     }
 
     private fun getCurrentLocation() {
@@ -70,7 +80,7 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
             return
         }
-        Toast.makeText(this, "made it", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, "made it", Toast.LENGTH_SHORT).show()
         fetchLocation()
     }
 
@@ -104,10 +114,17 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
             if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                Toast.makeText(this, "made it", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Location permission granted", Toast.LENGTH_SHORT).show()
                 fetchLocation()
             } else {
                 Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+        if (requestCode == NOTIFICATION_PERMISSION_REQUEST_CODE) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                Toast.makeText(this, "Notification permission granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Notification permission denied", Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -138,6 +155,8 @@ class MainActivity : AppCompatActivity() {
         getCurrentLocation()
 
         map = binding.map
+        val sharedPreferences = this.getSharedPreferences("osmdroidPrefs", Context.MODE_PRIVATE)
+        Configuration.getInstance().load(this, sharedPreferences)
         map.setTileSource(TileSourceFactory.MAPNIK)
 
         val mapController = map.controller
@@ -145,11 +164,27 @@ class MainActivity : AppCompatActivity() {
         val startPoint = GeoPoint(48.8583, 2.2944)
         mapController.setCenter(startPoint)
 
+        //map.setBuiltInZoomControls(true)
+        map.zoomController.setVisibility(CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT)
         map.setMultiTouchControls(true)
 
         app.loadFromFile()
         for (bird in app.birdList) {
             addMarker(bird)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.POST_NOTIFICATIONS), NOTIFICATION_PERMISSION_REQUEST_CODE)
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel("bird_channel", "Bird Notifications", NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = "Notifications for frequent bird sightings"
+            }
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
         }
     }
 
@@ -159,5 +194,48 @@ class MainActivity : AppCompatActivity() {
         marker.position = point
         marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
         map.overlays.add(marker)
+    }
+
+    fun getSightingsCount(birdName: String): Int {
+        var counter = 0
+        for (bird in app.birdList) {
+            if (bird.species == birdName) {
+                counter++
+            }
+        }
+        return counter
+    }
+
+    fun checkAndNotifyForFrequentBirds(birdName: String) {
+        val sightings = getSightingsCount(birdName)
+        val cycleLength = 3
+        if (sightings % cycleLength == 0 && sightings != 0) {
+            showNotification(birdName)
+        }
+    }
+
+    fun showNotification(birdName: String) {
+        val intent = Intent(this, ListActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntentFlags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        } else {
+            PendingIntent.FLAG_UPDATE_CURRENT
+        }
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, pendingIntentFlags)
+
+        val notificationBuilder = NotificationCompat.Builder(this, "bird_channel")
+            .setSmallIcon(R.drawable.crow)
+            .setContentTitle("Bird Watching Alert")
+            .setContentText("$birdName is hot right now!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(birdName.hashCode(), notificationBuilder.build())
+        }
     }
 }
